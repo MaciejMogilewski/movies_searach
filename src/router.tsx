b964fdf2ac37f4baefd91a5814e8b30049373a4c
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-05-06 13:18:08 +0200

    Configure react router dom

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Home</h1>,
+    },
+    {
+        path: "/movies/:movieId",
+        element: <h1>Movie</h1>,
+    },
+]);
